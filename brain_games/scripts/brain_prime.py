d849e03c350f1507d034faae6a810c0ff3e227ef
tgafiulin <t.gafiulin@ya.ru>
2024-04-09 15:59:12 +0300

    step 9

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import math
+import random
+
+from brain_games.startGame import startGame
+
+
+def main():
+    question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    startGame(getRoundData, question)
+
+
+def getRoundData():
+    number = random.randint(0, 99)
+    isPrime = checkPrimeNumber(number)
+    question = str(number)
+    rightAnswer = 'yes' if isPrime else 'no'
+    return (rightAnswer, question)
+
+
+def checkPrimeNumber (number):
+    if number == 1:
+        return False
+    elif number > 1:
+        for i in range(2, int(math.sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+            
+    return True
+
+
+if __name__ == '__main__':
+    main()
